3bb844d6660020a86e40e871446612aaf971f2b9
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-09-23 01:32:39 +0300

    Done

--- src/Parent.js
+++ src/Parent.js
@@ -2,16 +2,39 @@ import React, { useState } from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
-function Parent() {
+// function Parent() {
+//   const randomColor = getRandomColor();
+//   const [color, setColor] = useState(randomColor);  // initial value for color state
+
+function Parent () {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState('FFF'); // This represents the color of the two child components lives in Parent -- now we pass the data down as props
 
   return (
-    <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+    <div className = 'parent' style= {{backgroundColor: color}}>
+      <Child color = {childrenColor} onChangeColor={handleChangeColor}/>
+      <Child color = {childrenColor} onChangeColor= {handleChangeColor}/>
     </div>
-  );
+  )
+  function handleChangeColor(newChildColor){
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  }
 }
+// }
+//   function handleChangeColor() {
+//     const newRandomColor = getRandomColor();
+//     setColor(newRandomColor); // update color state to a new value
+//   }
+
+  // return (
+  //   <div className="parent" style={{ backgroundColor: color }}>
+  //     <Child onChangeColor={handleChangeColor} />
+  //     <Child onChangeColor={handleChangeColor} />
+  //   </div>
+  // );
+// }
 
 export default Parent;
